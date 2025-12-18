a8f28bac2f6d5c90636c32bc8f7ebc6ab349d8eb
Mujeeb Ur Rehman <mujeebtechcode@gmail.com>
2025-12-19 01:33:48 +0500

    change-code

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 import java . util.Scanner;
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Check paper");
+        System.out.println("Roommates calculation system:");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of bio:");
+        System.out.println("Shoiab:");
         int m1 = sc.nextInt();
-        System.out.println("Enter the number if English:");
+        System.out.println("Mujeeb:");
         int m2 = sc.nextInt();
-        System.out.println("Enter the number of physics:");
+        System.out.println("Adil:");
         int m3 = sc.nextInt();
 
         float avg =(m1+m2+m3)/3.0f;
         System.out.println("Total Average is "+avg);
 
-        if(avg>=33){
-            System.out.println("You promoted");
+        if(avg>=0){
+            System.out.println("totel bill");
         }
         else {
-            System.out.println("you not promoted");
+            System.out.println("Bill not be correct");
         }
     }
 }
