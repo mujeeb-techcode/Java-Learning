895602c09ce03e0296cdf9671ac1453bb04f6420
Mujeeb ur Rehman <m.mujeebtech@gmail.com>
2025-12-23 01:01:10 +0500

    Practice

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java .util.Scanner;
 public class Main{
     public static void main(String[] args) {
-        System.out.println("mujeeb ur rehman");
+        System.out.println();
+        Scanner sc = new Scanner (System.in);
+        int n=4;
 
+       // for(int i=n;i>=1;i--){
+            //for (int j = 1; j <= i;j++){
+               // System.out.print("*");
+            //}
+           // System.out.println();
+
+            for (int a =n;a>=1;a--){
+                for(int b= 1;b<=a;b++)
+                System.out.print("mujeeb");
+            }
+            System.out.println();
+
+
+        }
     }
-}
