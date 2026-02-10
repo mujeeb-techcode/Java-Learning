cad012712a5131269c8803bcd7751f9ea10591ef
Mujeeb ur Rehman <m.mujeebtech@gmail.com>
2026-02-10 20:54:24 +0500

    practice

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,10 @@
-import java .util.Scanner;
-public class Main{
-    public static void main(String[] args) {
+public class Main {
+    public static void main (String [] args ){
         System.out.println();
-        Scanner sc = new Scanner (System.in);
-        int n=4;
+      double a= 6565;
+      double b= 767;
 
-       // for(int i=n;i>=1;i--){
-            //for (int j = 1; j <= i;j++){
-               // System.out.print("*");
-            //}
-           // System.out.println();
+        System.out.println(a+b/a*b);
 
-            for (int a =n;a>=1;a--){
-                for(int b= 1;b<=a;b++)
-                System.out.print("mujeeb");
-            }
-            System.out.println();
-
-
-        }
     }
+}
