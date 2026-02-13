e4ed281b79378bd69ce2b733aa5d8bfce775cf58
Mujeeb ur Rehman <m.mujeebtech@gmail.com>
2026-02-13 20:04:29 +0500

    practice

---
+++ src/practice.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class practice {
+    public static void practice(String[] args) {
+        System.out.println("Marks calculation");
+        Scanner n = new Scanner (System.in);
+        System.out.println("<-----Enter the Marks taken in bio:");
+        int a= n.nextInt();
+        System.out.println("<-----Enter the Marks taken in Chemistry :");
+        int b = n.nextInt();
+
+        System.out.println("<-----Enter the marka taken in urdu:");
+        int c= n.nextInt();
+
+        System.out.println("<-----Number in math:");
+        int d = n .nextInt();
+
+        System.out.println("<-----Number in English:");
+        int e = n.nextInt();
+        System.out.println("The total number are all subjects :");
+        System.out.println((a+b+c+d+e)+"    Out of 500");
+    }
+}
