31e62e36e12a5a8a1052d87a7ba10df4ff946521
Mujeeb ur Rehman <m.mujeebtech@gmail.com>
2026-02-13 19:58:35 +0500

    learning

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
-public class Main {
-    public static void main (String [] args ){
-        System.out.println();
-      double a= 6565;
-      double b= 767;
+import java.util.Scanner;
 
-        System.out.println(a+b/a*b);
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Marks calculation");
+        Scanner n = new Scanner (System.in);
+        System.out.println("<-----Enter the Marks taken in bio:");
+        int a= n.nextInt();
+        System.out.println("<-----Enter the Marks taken in Chemistry :");
+        int b = n.nextInt();
+
+        System.out.println("<-----Enter the marka taken in urdu:");
+        int c= n.nextInt();
+
+        System.out.println("<-----Number in math:");
+        int d = n .nextInt();
+
+        System.out.println("<-----Number in English:");
+        int e = n.nextInt();
+        System.out.println("The total number are all subjects :");
+        System.out.println((a+b+c+d+e)+"    Out of 500");
 
     }
 }
